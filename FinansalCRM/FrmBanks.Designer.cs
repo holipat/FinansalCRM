137f3e0ba9fd7d1281c9be5229dd30a0a9e20b0f
Aleyna <bergamaleyna@gmail.com>
2025-03-16 13:48:01 +0300

    first form completed

--- FinansalCRM/FrmBanks.Designer.cs
+++ FinansalCRM/FrmBanks.Designer.cs
@@ -49,15 +49,15 @@
             this.lblKuveytTurkBalance = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
-            this.label11 = new System.Windows.Forms.Label();
+            this.lblBankProcess5 = new System.Windows.Forms.Label();
+            this.lblBankProcess4 = new System.Windows.Forms.Label();
             this.label12 = new System.Windows.Forms.Label();
-            this.label13 = new System.Windows.Forms.Label();
+            this.lblBankProcess3 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.lblBankProcess2 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblBankProcess1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
@@ -93,7 +93,6 @@
             this.button8.TabIndex = 7;
             this.button8.Text = "Çıkış Yap";
             this.button8.UseVisualStyleBackColor = false;
-            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button7
             // 
@@ -120,7 +119,6 @@
             this.button6.TabIndex = 5;
             this.button6.Text = "Dashboard";
             this.button6.UseVisualStyleBackColor = false;
-            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button5
             // 
@@ -207,7 +205,6 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Banka Formu";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
@@ -218,7 +215,6 @@
             this.label2.Size = new System.Drawing.Size(171, 33);
             this.label2.TabIndex = 0;
             this.label2.Text = "Ziraat Bankası";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // panel3
             // 
@@ -241,7 +237,6 @@
             this.lblZiraatBankBalance.Size = new System.Drawing.Size(229, 59);
             this.lblZiraatBankBalance.TabIndex = 1;
             this.lblZiraatBankBalance.Text = "00000,00₺";
-            this.lblZiraatBankBalance.Click += new System.EventHandler(this.label3_Click);
             // 
             // panel4
             // 
@@ -306,19 +301,18 @@
             this.label7.Size = new System.Drawing.Size(142, 33);
             this.label7.TabIndex = 0;
             this.label7.Text = "KuveytTürk";
-            this.label7.Click += new System.EventHandler(this.label7_Click);
             // 
             // groupBox1
             // 
             this.groupBox1.BackColor = System.Drawing.Color.LightSteelBlue;
-            this.groupBox1.Controls.Add(this.label13);
-            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.lblBankProcess5);
+            this.groupBox1.Controls.Add(this.lblBankProcess4);
             this.groupBox1.Controls.Add(this.label12);
-            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.lblBankProcess3);
             this.groupBox1.Controls.Add(this.label10);
-            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.lblBankProcess2);
             this.groupBox1.Controls.Add(this.label8);
-            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.lblBankProcess1);
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Font = new System.Drawing.Font("Calibri", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.groupBox1.Location = new System.Drawing.Point(252, 257);
@@ -328,76 +322,23 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Son 5 Banka Hareketi";
             // 
-            // label3
+            // lblBankProcess5
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 64);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(129, 42);
-            this.label3.TabIndex = 0;
-            this.label3.Text = "Metin 1";
-            this.label3.Click += new System.EventHandler(this.label3_Click_2);
+            this.lblBankProcess5.AutoSize = true;
+            this.lblBankProcess5.Location = new System.Drawing.Point(6, 356);
+            this.lblBankProcess5.Name = "lblBankProcess5";
+            this.lblBankProcess5.Size = new System.Drawing.Size(129, 42);
+            this.lblBankProcess5.TabIndex = 8;
+            this.lblBankProcess5.Text = "Metin 1";
             // 
-            // label4
+            // lblBankProcess4
             // 
-            this.label4.AutoSize = true;
-            this.label4.BackColor = System.Drawing.Color.Transparent;
-            this.label4.Location = new System.Drawing.Point(2, 85);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(868, 42);
-            this.label4.TabIndex = 1;
-            this.label4.Text = "__________________________________________________";
-            this.label4.Visible = false;
-            this.label4.Click += new System.EventHandler(this.label4_Click);
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(6, 137);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(129, 42);
-            this.label6.TabIndex = 2;
-            this.label6.Text = "Metin 1";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.BackColor = System.Drawing.Color.Transparent;
-            this.label8.Location = new System.Drawing.Point(2, 158);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(868, 42);
-            this.label8.TabIndex = 3;
-            this.label8.Text = "__________________________________________________";
-            this.label8.Visible = false;
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(6, 210);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(129, 42);
-            this.label9.TabIndex = 4;
-            this.label9.Text = "Metin 1";
-            // 
-            // label10
-            // 
-            this.label10.AutoSize = true;
-            this.label10.BackColor = System.Drawing.Color.Transparent;
-            this.label10.Location = new System.Drawing.Point(2, 231);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(868, 42);
-            this.label10.TabIndex = 5;
-            this.label10.Text = "__________________________________________________";
-            this.label10.Visible = false;
-            // 
-            // label11
-            // 
-            this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(6, 283);
-            this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(129, 42);
-            this.label11.TabIndex = 6;
-            this.label11.Text = "Metin 1";
+            this.lblBankProcess4.AutoSize = true;
+            this.lblBankProcess4.Location = new System.Drawing.Point(6, 283);
+            this.lblBankProcess4.Name = "lblBankProcess4";
+            this.lblBankProcess4.Size = new System.Drawing.Size(129, 42);
+            this.lblBankProcess4.TabIndex = 6;
+            this.lblBankProcess4.Text = "Metin 1";
             // 
             // label12
             // 
@@ -410,14 +351,65 @@
             this.label12.Text = "__________________________________________________";
             this.label12.Visible = false;
             // 
-            // label13
+            // lblBankProcess3
             // 
-            this.label13.AutoSize = true;
-            this.label13.Location = new System.Drawing.Point(6, 356);
-            this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(129, 42);
-            this.label13.TabIndex = 8;
-            this.label13.Text = "Metin 1";
+            this.lblBankProcess3.AutoSize = true;
+            this.lblBankProcess3.Location = new System.Drawing.Point(6, 210);
+            this.lblBankProcess3.Name = "lblBankProcess3";
+            this.lblBankProcess3.Size = new System.Drawing.Size(129, 42);
+            this.lblBankProcess3.TabIndex = 4;
+            this.lblBankProcess3.Text = "Metin 1";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.Location = new System.Drawing.Point(2, 231);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(868, 42);
+            this.label10.TabIndex = 5;
+            this.label10.Text = "__________________________________________________";
+            this.label10.Visible = false;
+            // 
+            // lblBankProcess2
+            // 
+            this.lblBankProcess2.AutoSize = true;
+            this.lblBankProcess2.Location = new System.Drawing.Point(6, 137);
+            this.lblBankProcess2.Name = "lblBankProcess2";
+            this.lblBankProcess2.Size = new System.Drawing.Size(129, 42);
+            this.lblBankProcess2.TabIndex = 2;
+            this.lblBankProcess2.Text = "Metin 1";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Location = new System.Drawing.Point(2, 158);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(868, 42);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "__________________________________________________";
+            this.label8.Visible = false;
+            // 
+            // lblBankProcess1
+            // 
+            this.lblBankProcess1.AutoSize = true;
+            this.lblBankProcess1.Location = new System.Drawing.Point(6, 64);
+            this.lblBankProcess1.Name = "lblBankProcess1";
+            this.lblBankProcess1.Size = new System.Drawing.Size(129, 42);
+            this.lblBankProcess1.TabIndex = 0;
+            this.lblBankProcess1.Text = "Metin 1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Location = new System.Drawing.Point(2, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(868, 42);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "__________________________________________________";
+            this.label4.Visible = false;
             // 
             // FrmBanks
             // 
@@ -475,13 +467,13 @@
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label13;
-        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label lblBankProcess1;
+        private System.Windows.Forms.Label lblBankProcess5;
+        private System.Windows.Forms.Label lblBankProcess4;
         private System.Windows.Forms.Label label12;
-        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lblBankProcess3;
         private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblBankProcess2;
         private System.Windows.Forms.Label label8;
     }
 }
--- FinansalCRM/FrmBanks.cs
+++ FinansalCRM/FrmBanks.cs
@@ -14,38 +14,9 @@ namespace FinansalCRM
 
         FinansalCrmDbEntities1 db = new FinansalCrmDbEntities1();
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button6_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button8_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label7_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label3_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void FrmBanks_Load(object sender, EventArgs e)
         {
+            //Banka Bakiyeleri
             var ziraatBankBalance = db.Banks.Where(x => x.BankTitle == "Ziraat Bankası").Select(y=>y.BankBalance).FirstOrDefault();
             var vakifBankBalance = db.Banks.Where(x => x.BankTitle == "Vakıf Katılım").Select(y => y.BankBalance).FirstOrDefault();
             var kuveytturkBankBalance = db.Banks.Where(x => x.BankTitle == "KuveytTürk").Select(y => y.BankBalance).FirstOrDefault();
@@ -53,21 +24,27 @@ namespace FinansalCRM
             lblZiraatBankBalance.Text = ziraatBankBalance.ToString() + " ₺";
             lblVakifbankBalance.Text = vakifBankBalance.ToString() + " ₺";
             lblKuveytTurkBalance.Text = kuveytturkBankBalance.ToString() + " ₺";
-        }
 
-        private void label3_Click_1(object sender, EventArgs e)
-        {
+            //Banka Hareketleri
+            var bankProcess1 = db.BankProcesses.OrderByDescending(x=>x.BankProcessId).Take(1).FirstOrDefault();
+            DateTime? process1Date = bankProcess1.ProcessDate;
+            lblBankProcess1.Text = bankProcess1.Description + " | " + bankProcess1.Amount + " ₺ | " + process1Date?.ToString("dd.MM.yyyy");
 
-        }
+            var bankProcess2 = db.BankProcesses.OrderByDescending(x => x.BankProcessId).Take(2).Skip(1).FirstOrDefault();
+            DateTime? process2Date = bankProcess2.ProcessDate;
+            lblBankProcess2.Text = bankProcess2.Description + " | " + bankProcess2.Amount + " ₺ | " + process2Date?.ToString("dd.MM.yyyy");
 
-        private void label4_Click(object sender, EventArgs e)
-        {
+            var bankProcess3 = db.BankProcesses.OrderByDescending(x => x.BankProcessId).Take(3).Skip(2).FirstOrDefault();
+            DateTime? process3Date = bankProcess3.ProcessDate;
+            lblBankProcess3.Text = bankProcess3.Description + " | " + bankProcess3.Amount + " ₺ | " + process3Date?.ToString("dd.MM.yyyy");
 
-        }
+            var bankProcess4 = db.BankProcesses.OrderByDescending(x => x.BankProcessId).Take(4).Skip(3).FirstOrDefault();
+            DateTime? process4Date = bankProcess4.ProcessDate;
+            lblBankProcess4.Text = bankProcess4.Description + " | " + bankProcess4.Amount + " ₺ | " + process4Date?.ToString("dd.MM.yyyy");
 
-        private void label3_Click_2(object sender, EventArgs e)
-        {
-
+            var bankProcess5 = db.BankProcesses.OrderByDescending(x => x.BankProcessId).Take(5).Skip(4).FirstOrDefault();
+            DateTime? process5Date = bankProcess5.ProcessDate;
+            lblBankProcess5.Text = bankProcess5.Description + " | " + bankProcess5.Amount + " ₺ | " + process5Date?.ToString("dd.MM.yyyy");
         }
     }
 }
